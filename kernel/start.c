e40f7c71d1e3e929e84930e2bd3b3fb724f14bc6
mrm <cmpl.error@gmail.com>
2021-12-30 02:16:59 +0900

    fix start.c

--- kernel/start.c
+++ kernel/start.c
@@ -3,58 +3,21 @@
 #include "memlayout.h"
 #include "aarch64.h"
 #include "defs.h"
+#include "psci.h"
 
 void main();
-void timerinit();
 
 // entry.S needs one stack per CPU.
 __attribute__ ((aligned (16))) char stack0[4096 * NCPU];
-
-// a scratch area per CPU for machine-mode timer interrupts.
-uint64 timer_scratch[NCPU][5];
-
-// assembly code in kernelvec.S for machine-mode timer interrupt.
-extern void timervec();
 
 // entry.S jumps here in supervisor mode (EL1) on stack0.
 // in qemu-system-aarch64, default EL (Exeception Level) is 1.
 void
 start()
 {
+  // wakeup other cpu
+  for(int id = 1; id < NCPU; id++)
+    psci_cpuon(id, (uint64)main);
+
   main();
-}
-
-// set up to receive timer interrupts in machine mode,
-// which arrive at timervec in kernelvec.S,
-// which turns them into software interrupts for
-// devintr() in trap.c.
-void
-timerinit()
-{
-  /*
-  // each CPU has a separate source of timer interrupts.
-  int id = cpuid();
-
-  // ask the CLINT for a timer interrupt.
-  int interval = 1000000; // cycles; about 1/10th second in qemu.
-  *(uint64*)CLINT_MTIMECMP(id) = *(uint64*)CLINT_MTIME + interval;
-
-  // prepare information in scratch[] for timervec.
-  // scratch[0..2] : space for timervec to save registers.
-  // scratch[3] : address of CLINT MTIMECMP register.
-  // scratch[4] : desired interval (in cycles) between timer interrupts.
-  uint64 *scratch = &timer_scratch[id][0];
-  scratch[3] = CLINT_MTIMECMP(id);
-  scratch[4] = interval;
-  w_mscratch((uint64)scratch);
-
-  // set the machine-mode trap handler.
-  w_mtvec((uint64)timervec);
-
-  // enable machine-mode interrupts.
-  w_mstatus(r_mstatus() | MSTATUS_MIE);
-
-  // enable machine-mode timer interrupts.
-  w_mie(r_mie() | MIE_MTIE);
-  */
 }
